a7fd83652f81f5a3a6c8f4055d952db0b42c4a37
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-04-04 15:57:29 -0300

    Add files via upload

---
+++ 03_tiposDeVariaveis.rb
@@ -0,0 +1,14 @@
+# conhecendo os tipos de variáveis
+
+# string
+language = "Ruby"
+puts language.class     # para conhecer o tipo de variável, utilizamos a função class
+
+# integer
+year = 2021
+puts year.class
+
+# float
+pi = 3.14
+puts pi.class
+
