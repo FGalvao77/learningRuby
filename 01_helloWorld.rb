7539d76e618c1dfa7312d48f056c7baca6da3bf3
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-04-04 15:56:42 -0300

    Add files via upload

---
+++ 01_helloWorld.rb
@@ -0,0 +1,9 @@
+# imprimindo "Hello, World!"
+
+puts "Hello, World!"
+
+# existem outras duas funções para imprimir algo
+
+p "Hello, World!"       # p - deixa a mensagem entre aspas
+print "Hello, World!"   # print - não pula linha
+puts " Hello, World!"    # puts - pula linha
