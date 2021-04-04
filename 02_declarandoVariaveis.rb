e3587bb1abe10dedd2377250bdde8bc10b7dba18
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-04-04 15:57:07 -0300

    Add files via upload

---
+++ 02_declarandoVariaveis.rb
@@ -0,0 +1,13 @@
+# declarando variáveis
+firstName = "Fernando"
+puts firstName
+
+# concatenando variáveis
+lastName = "Galvão"
+
+puts "Full name: #{firstName} #{lastName}"
+
+age = 39
+
+print "\n\tPersonal data"
+puts "\n\t\tFull Name: #{firstName} #{lastName} \n\t\tAge: #{age}"
